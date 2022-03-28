e7cf84d355a5668cf01eaad24294432f3267a11c
Alamega <vladshuman@gmail.com>
2022-03-28 19:02:40 +0300

    task1-1 comlete

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-console.log("Hello?");
+#!/usr/bin/env node
+
+function duplicateEncode(word) {
+    word = word.toLowerCase();
+    var word = word.split('');
+    var result = "";
+    for (let i = 0; i < word.length; i++) {
+        if (word.indexOf(word[i])==word.lastIndexOf(word[i])) {
+           result+=word[i];
+        }
+        else
+        {
+            result+=")";
+        }
+    }
+    word = result.split('');
+    result = "";
+    for (let i = 0; i < word.length; i++) {
+            if (word[i]!=")") {
+                result+="(";
+            }
+            else
+            {
+                result+=word[i];
+            }
+
+    }
+    console.log(result);
+}
+
+duplicateEncode("din") // -> "((("
+duplicateEncode("recede") // -> "()()()"
+duplicateEncode("Success") // -> ")())())"
+duplicateEncode("(( @") // -> "))(("
